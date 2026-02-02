5fea9527df7bd5d746974b2f3b905375486563e6
Paulo Varassin <pauloarthurvm@hotmail.com>
2026-02-02 19:50:02 -0300

    Standard files

---
+++ src/test/java/com/pv/mylibrary/MylibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pv.mylibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MylibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
